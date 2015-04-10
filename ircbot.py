7d1b2ab73b0b7596471a00ca6cb55b72d932b98b
Ryan Quinn <fowardingtoyou@gmail.com>
2015-04-10 15:37:41 -0400

    Class-ified IRCBot and cleaned up code

--- ircbot.py
+++ ircbot.py
@@ -1,59 +1,59 @@
 import socket
 import time
 
-server = "cho.ppy.sh"
-name = "ApolloFortyNine"
-port = 6667
-channel = "#Osu"
-password = "784062"
 
-def send(msg):
-    print("I>" + msg)
-    socket.send(bytes(msg+"\r\n", 'UTF-8'))
+class IRCBot:
+    def __init__(self, server, nickname, port, channel, password):
+        self.socket = socket.socket()
+        self.server = server
+        self.nickname = nickname
+        self.port = port
+        self.channel = channel
+        self.password = password
 
-socket = socket.socket()
-socket.connect((server, port))
-send("PASS 784062\r\n")
-send("NICK " + name + "\r\n")
-send("USER ApolloFortyNine ApolloFortyNine Ryan :ApolloFortyNine \r\n")
-duerig = True
-stuff = ""
-start = time.time()
-begin_names = False
-while (time.time() - start) < 10:
-        buf = socket.recv(4096)
-        lines = buf.split(b'\n')
-        for data in lines:
-            data = str(data.decode('utf-8')).strip()
-            if data == '':
-                continue
-            # This will handle when a line wasn't finished being received
-            if (data.find(":cho.ppy.sh 353 ApolloFortyNine = OSU :") != -1) | begin_names:
-                begin_names = True
-                if data.find("End of /NAMES list.") == -1:
-                    stuff += data
-                else:
-                    begin_names = False
-            print("I<" + data)
-            if data.find('PING') != -1:
-                n = data.split(':')[1]
-                send('PONG :' + n)
-            #if connected == False:
-               # perform()
-               # connected = True
-        if ((time.time() - start) > 5) & duerig:
-            duerig = False
-            send("NAMES OSU")
-time.sleep(1)
-stuff = stuff.replace(":cho.ppy.sh 353 ApolloFortyNine = OSU :", "")
-stuff = stuff.replace("@", "")
-stuff = stuff.replace("+", "")
-print(stuff)
-#    def perform():
-        #self.send("PRIVMSG R : Register <>")
-#        send("PRIVMSG R : Login <>")
-#        send("MODE %s +x" % nickname)
-#        for c in self.channels:
-#            send("JOIN %s" % c)
-#            # say hello to every channel
-#            say('hello world!', c)
+    def send(self, msg):
+        self.socket.send(bytes(msg+"\r\n", 'UTF-8'))
+
+    def get_names(self):
+        self.socket.connect((self.server, self.port))
+        time.sleep(.05)
+        self.send("PASS " + self.password + "\r\n")
+        time.sleep(.05)
+        self.send("NICK " + self.nickname + "\r\n")
+        time.sleep(.05)
+        self.send("USER " + self.nickname + " " + self.nickname + " " + self.nickname + " :" + self.nickname + "\r\n")
+        time.sleep(.05)
+        raw_names = ""
+        start = time.time()
+        begin_read_names = False
+        send_names_bool = True
+
+        while (time.time() - start) < 4:
+            buffer = self.socket.recv(4096)
+            lines = buffer.split(b'\n')
+
+            for data in lines:
+                data = str(data.decode('utf-8'))
+                if data == '':
+                    continue
+                # This will handle when a line wasn't finished being received
+                if (data.find(":cho.ppy.sh 353 ApolloFortyNine = OSU :") != -1) | begin_read_names:
+                    begin_read_names = True
+                    if data.find("End of /NAMES list.") == -1:
+                        raw_names += data
+                    else:
+                        begin_read_names = False
+                if data.find('PING') != -1:
+                    n = data.split(':')[1]
+                    self.send('PONG :' + n)
+            if ((time.time() - start) > 2) & send_names_bool:
+                send_names_bool = False
+                self.send("NAMES " + self.channel)
+
+        time.sleep(.05)
+        # Remove junk data
+        raw_names = raw_names.replace(":cho.ppy.sh 353 ApolloFortyNine = OSU :", "")
+        raw_names = raw_names.replace("@", "")
+        raw_names = raw_names.replace("+", "")
+        names_list = raw_names.split(" ")
+        return names_list

a6cfdf0182abdf79ea11fc49905126b8e8d10943
Ryan Quinn <fowardingtoyou@gmail.com>
2015-04-11 02:44:30 -0400

    Initial web script

---
+++ webtest.py
@@ -0,0 +1,24 @@
+from getFriend import GetFriend
+import cherrypy
+
+
+class StringGenerator(object):
+    @cherrypy.expose
+    def index(self):
+        return """<html>
+          <head></head>
+          <body>
+            <form method="get" action="get_friend">
+              <input type="text" value="HappyStick" name="name" />
+              <button type="submit">Your username</button>
+            </form>
+          </body>
+        </html>"""
+
+    @cherrypy.expose
+    def get_friend(self, name="HappyStick"):
+        friend = GetFriend(name)
+        return friend.username
+
+if __name__ == '__main__':
+    cherrypy.quickstart(StringGenerator())

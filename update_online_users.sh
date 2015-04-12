dc141df3f29363ecb393ea316ce9891420883427
Ryan Quinn <fowardingtoyou@gmail.com>
2015-04-12 01:24:56 -0400

    Moved everything back to the same directory

--- update_online_users.sh
+++ update_online_users.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Replace path with path to your git clone, then add to crontab
-screen -d -m -S scrape python3 #PATH/scripts/update_online_users.py
+screen -d -m -S scrape python3 #PATH/update_online_users.py

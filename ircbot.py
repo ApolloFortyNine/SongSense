2c902e53e9188a8bc97ee2172c929362ba5e244d
Ryan Quinn <fowardingtoyou@gmail.com>
2015-04-14 21:51:18 -0400

    Extended !h message

--- ircbot.py
+++ ircbot.py
@@ -79,8 +79,9 @@ class IRCBot:
                             map_str = self.get_map_str(friend)
                             message = "Random Recommendation: " + map_str
                         elif payload['msg'] == '!h':
-                            message = "Welcome to Osu Friend Finder! Type \"!f\" to find your number one friend who shares beatmaps with " \
-                                      "you and \"!r\" for a recommendation!"
+                            message = "Welcome to Osu Friend Finder! Type \"!f\" to find your number one friend who " \
+                                      "shares beatmaps with you and \"!r\" for a recommendation! \"!rX\" where x is a " \
+                                      "number 1-10 also works."
                         elif payload['msg'] == '!f':
                             friend = GetFriend(payload['sender'])
                             message = ("Your best friend: " + str(friend.friend_url) + " with " + str(friend.matches) +

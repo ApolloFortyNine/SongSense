c29efd107e01f46df9dbc508450b7bc4930c486b
Ryan Quinn <fowardingtoyou@gmail.com>
2015-04-20 00:45:04 -0400

    Added script that will remove old friends

---
+++ delete_old_friends.py
@@ -0,0 +1,17 @@
+"""
+This script will remove friends older than 3 days. These friends are updated every 3 days anyways,
+so they're no longer needed.
+"""
+from sqlalchemy.orm import sessionmaker
+from config import Config
+from database import *
+import datetime
+
+config = Config()
+engine = create_engine(config.engine_str, **config.engine_args)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+session.query(Friend).filter(Friend.last_updated < (datetime.datetime.now() -
+                                                    datetime.timedelta(days=3))).delete()
+session.commit()

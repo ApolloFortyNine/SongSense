240a31211b2308db1c2bb607c9d9740877437132
Ryan Quinn <fowardingtoyou@gmail.com>
2015-04-21 00:31:08 -0400

    Added absolute_import

--- songsense/__init__.py
+++ songsense/__init__.py
@@ -1,5 +1,6 @@
 import os
 import os.path
+from __future__ import absolute_import
 
 package_dir = os.path.dirname(__file__)
 git_dir = os.path.normpath(os.path.join(package_dir, os.pardir, os.pardir))

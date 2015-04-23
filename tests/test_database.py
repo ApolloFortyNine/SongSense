5ee9ffb002a89a9cef357debcb6346079ce10875
Ryan Quinn <fowardingtoyou@gmail.com>
2015-04-22 21:22:58 -0400

    Added test for friend name

--- tests/test_database.py
+++ tests/test_database.py
@@ -13,9 +13,18 @@ session = Session()
 def setup_module(module):
     filler = fill.Fill(engine)
     filler.fill_data('ApolloFortyNine')
+    filler.fill_data('gelibolue')
 
 
 def test_fill():
     user = session.query(User).filter(User.username == 'ApolloFortyNine').first()
     if not user:
         assert False
+    session.query(User).filter(User.username == 'ApolloFortyNine').delete()
+    session.commit()
+
+
+def test_friend_name():
+    friend = getfriend.GetFriend('ApolloFortyNine')
+    name = friend.get_friend_name()
+    assert name == 'gelibolue'

471f60d4ac041407fe3c9b6bbd0efbedb8baefe8
Ryan Quinn <fowardingtoyou@gmail.com>
2015-04-22 21:12:53 -0400

    Test if setup_module works as I believe

--- tests/test_database.py
+++ tests/test_database.py
@@ -4,14 +4,18 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import *
 import config
 
+my_config = config.Config()
+engine = create_engine(my_config.engine_str, **my_config.engine_args)
+Session = sessionmaker(engine)
+session = Session()
 
-def test_fill():
-    my_config = config.Config()
-    engine = create_engine(my_config.engine_str, **my_config.engine_args)
-    Session = sessionmaker(engine)
-    session = Session()
+
+def setup_module(module):
     filler = fill.Fill(engine)
     filler.fill_data('ApolloFortyNine')
+
+
+def test_fill():
     user = session.query(User).filter(User.username == 'ApolloFortyNine').first()
     if not user:
         assert False

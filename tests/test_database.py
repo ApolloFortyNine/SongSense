496e8aa33203e94d16ae8288a24d76c55f9e1b25
Ryan Quinn <fowardingtoyou@gmail.com>
2015-04-29 04:55:13 -0400

    Pushed branch with config by accident

--- tests/test_database.py
+++ tests/test_database.py
@@ -2,9 +2,9 @@ from songsense import fill, getfriend
 from songsense.database import  *
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import *
-import config
+from songsense.config import Config
 
-my_config = config.Config()
+my_config = Config()
 engine = create_engine(my_config.engine_str, **my_config.engine_args)
 Session = sessionmaker(engine)
 session = Session()
